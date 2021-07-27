56d0377286d1befb19a9ce9f47c75fe7a290a7c4
TeemoTrader <austinnorman1995@gmail.com>
2021-07-27 13:24:07 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Teemo Norman on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Teemo Norman on 7/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
